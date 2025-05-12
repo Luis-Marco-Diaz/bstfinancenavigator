077025d50d2405211e0897e7b8bda47fdae41957
Marco <marko.dzm@gmail.com>
2025-05-12 13:40:35 -0600

    BSTFinanceNavigator-V1

--- js/scripts.js
+++ js/scripts.js
@@ -53,3 +53,18 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+   // Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
